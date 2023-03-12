27a506c22e4b51774295399ae87cbfd15e290bb9
BRICE JOSHY <bricejoshy99@gmail.com>
2023-03-12 17:53:33 +0530

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/auth/login_screen.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  _initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -16,19 +19,22 @@ class MyApp extends StatelessWidget {
         title: 'Chatzon .',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          appBarTheme: AppBarTheme(
+          appBarTheme: const AppBarTheme(
             elevation: 1,
             centerTitle: true,
             backgroundColor: Colors.white,
-            iconTheme: IconThemeData(
-              color: Colors.black
-            ),
+            iconTheme: IconThemeData(color: Colors.black),
             titleTextStyle: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.normal,
                 fontSize: 19),
           ),
         ),
-        home: login_screen());
+        home: const splash_screen());
   }
+}
+
+_initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // now we need to call this function in main
 }
