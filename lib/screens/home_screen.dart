880d8660be9d020812ebaa256218d6ebfbedecac
BRICE JOSHY <bricejoshy99@gmail.com>
2023-03-14 22:15:34 +0530

    intergating firestore firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/chat_user_card.dart';
+import 'splash_screen.dart';
+
 // statefull widget as we are dynamically changing it
 class home_screen extends StatefulWidget {
   const home_screen({super.key});
@@ -34,6 +37,17 @@ class _home_screenState extends State<home_screen> {
         padding: const EdgeInsets.only(bottom: 10, right: 10),
         child: FloatingActionButton(
             onPressed: () {}, child: const Icon(Icons.add_comment_rounded)),
+      ),
+
+      // builds list item for every user
+      body: ListView.builder(
+        itemCount: 16,
+        padding: EdgeInsets.only(top: mq.height * .01),
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          // returning the chat user card
+          return const Chat_User_Card();
+        },
       ),
     );
   }
--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../screens/splash_screen.dart';
 
 class Chat_User_Card extends StatefulWidget {
   const Chat_User_Card({super.key});
@@ -10,6 +13,35 @@ class Chat_User_Card extends StatefulWidget {
 class _Chat_User_CardState extends State<Chat_User_Card> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: mq.width * .04, vertical: 4),
+      color: Colors.grey.shade100,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 0.5,
+      child: InkWell(
+        onTap: () {},
+        child: const ListTile(
+          //user profile picture
+          leading: CircleAvatar(
+            child: Icon(CupertinoIcons.person),
+          ),
+
+          //user name
+          title: Text('Demo User'),
+
+          //last message
+          subtitle: Text(
+            'Last Message',
+            maxLines: 1,
+          ),
+
+          //last message time
+          trailing: Text(
+            "12:00 PM",
+            style: TextStyle(color: Colors.black54),
+          ),
+        ),
+      ),
+    );
   }
 }
