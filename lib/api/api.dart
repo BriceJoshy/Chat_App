be93ac16634570806382a8e2f7e79b7c856d6b67
BRICE JOSHY <bricejoshy99@gmail.com>
2023-03-16 11:55:22 +0530

    Firestore

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,7 +1,13 @@
 // Creating a api class which has a static object for auth
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs {
-  static FirebaseAuth auth = FirebaseAuth.instance;
-  // specifying the type of the static object
+  // for authentication
+  static FirebaseAuth auth = FirebaseAuth.instance; // specifying the type of the static object
+  
+  // for accessing cloud firestore databse
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  
 }
